53da265a6797d879931ff8dbfe42abfecb080f40
cassjsdev <cassjes.m@gmail.com>
2021-04-27 19:32:13 -0700

    edited docker run line

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,28 +2,28 @@
 
 usage="Usage: $0 <VERSION TAG> <FLASK_ENV>"
 
-if [ "$1" ]
+if [[ "$1" ]]
 then
   CITY == $1
 else
   CITY == "Houston"
 fi
 
-if [ "$2" ]
+if [[ "$2" ]]
 then
   STATE == $2
 else
   STATE == "Texas"
 fi
 
-if [ "$3" ]
+if [[ "$3" ]]
 then
   STATE_ABBR == $3
 else
   STATE_ABBR == "TX"
 fi
 
-if [ "$4" ]
+if [[ "$4" ]]
 then
   ZIPCODE == $4
 else
@@ -31,7 +31,6 @@ else
 fi
 
 # Run Docker container
-docker run vaccine-watcher:latest --city=CITY --state=STATE --abbr=STATE_ABBR --zip=ZIPCODE
-# docker run --platform linux/arm64/v8 vaccine-watcher --city=CITY --state=STATE --abbr=STATE_ABBR --zip=ZIPCODE
+docker run --platform linux/arm64/v8 vaccine-watcher:latest --city=CITY --state=STATE --abbr=STATE_ABBR --zip=ZIPCODE
 
-#Example Run: ./scripts/run.sh --city "Woodland" --state "California" --abbr "CA" --zip "95776"
+# Example Run: ./scripts/run.sh --city "Woodland" --state "California" --abbr "CA" --zip "95776"
